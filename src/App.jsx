29f7808278a2a28f5e29afea3b87c60862a71853
Khadija12319 <khadijaatul15-12319@diu.edu.bd>
2024-03-18 16:12:31 +0600

    form side image added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
     <div className='bg-[#45FFCA] h-screen flex'>
-      <div className='w-[700px] bg-white h-[550px] grid m-auto place-items-center rounded-3xl'>
+      <div className='w-[900px] bg-white h-auto grid grid-cols-2 m-auto place-items-center rounded-3xl p-12'>
+        <img src="/src/assets/form-side.png" alt="" />
         <Form></Form>
       </div>
     </div>
